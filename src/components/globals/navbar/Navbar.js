ba30fad1683c06f35e37f99d18f509681f2f0de9
Art Bachmann <bachmann.art@gmail.com>
2019-05-21 14:09:19 +0300

    menu repaired, testing pwa

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -3,23 +3,20 @@ import NavbarHeader from "./NavbarHeader"
 import NavbarLinks from "./NavbarLinks"
 import NavbarIcons from "./NavbarIcons"
 import styled from "styled-components"
-
-class Navbar extends Component {
+export default class Navbar extends Component {
   state = {
     navbarOpen: false,
   }
-
   handleNavbar = () => {
     this.setState(() => {
       return { navbarOpen: !this.state.navbarOpen }
     })
   }
-
   render() {
     return (
       <NavWrapper>
         <NavbarHeader handleNavbar={this.handleNavbar} />
-        <NavbarLinks navbarOpen={this.navbarOpen} />
+        <NavbarLinks navbarOpen={this.state.navbarOpen} />
         <NavbarIcons />
       </NavWrapper>
     )
@@ -27,10 +24,8 @@ class Navbar extends Component {
 }
 
 const NavWrapper = styled.nav`
-  @media (min-width: 760px) {
+  @media (min-width: 768px) {
     display: flex;
     align-items: center;
   }
 `
-
-export default Navbar
