11cf0f13c9a87e793331bcd996e69641247beb31
Art Bachmann <bachmann.art@gmail.com>
2019-05-21 13:31:08 +0300

    added manifest.json and pwa capabilities, testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,18 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
 
